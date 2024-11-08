608998e88013d4a07fddeabbfca28d21d74133e3
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-09 00:04:42 +0900

    feat: 재고 관리에 필요한 상품 클래스 생성

---
+++ src/main/java/store/Product.java
@@ -0,0 +1,53 @@
+package store;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final String promotion;
+
+    public Product(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public static Product from(String name, int price, int quantity) {
+        return new Product(name, price, quantity, "");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean hasPromotion() {
+        return !(promotion.isEmpty());
+    }
+
+    public boolean isOutOfStock() {
+        return quantity == 0;
+    }
+
+    @Override
+    public String toString() {
+        String promotionText = "";
+        String quantityText = "재고 없음";
+        if(promotion != null){
+            promotionText = " " + promotion;
+        }
+        if(quantity != 0){
+            quantityText = quantity + "개";
+        }
+        String formattedPrice = String.format("%,d", price);
+        return "- " + name + " " + formattedPrice + "원 " + quantityText + promotionText;
+    }
+}
