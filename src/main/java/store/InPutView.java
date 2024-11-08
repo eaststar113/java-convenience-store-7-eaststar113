1891b89dfae7e44cd2fd722a4ff54225acb39fb4
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-08 23:51:20 +0900

    feat: 상품명과 수량 입력받는 기능 생성

---
+++ src/main/java/store/InPutView.java
@@ -0,0 +1,14 @@
+package store;
+
+import static store.InputMessage.NEXT_LINE;
+import static store.InputMessage.ORDER_MESSAGE;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InPutView {
+
+    public static String getOrder() {
+        System.out.println(NEXT_LINE.getMessage()+ORDER_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+}
