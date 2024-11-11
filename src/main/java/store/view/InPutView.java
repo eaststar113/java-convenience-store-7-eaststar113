fcdfb25543839c9fecddd08e79b16eaff44ec43b
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-11 18:18:39 +0900

    refactor: 영수증 출력 규격 맞게 조정

--- src/main/java/store/view/InPutView.java
+++ src/main/java/store/view/InPutView.java
@@ -8,24 +8,32 @@ import store.Product;
 
 public class InPutView {
 
-    public static String getOrder() {
+    public static String printOrderMessage() {
         System.out.println(NEXT_LINE.getMessage()+ORDER_MESSAGE.getMessage());
         return Console.readLine();
     }
 
-    public static String promotionStockAlertMessage(Product product, int outOfStockCount) {
+    public static String printPromotionStockAlertMessage(Product product, int outOfStockCount) {
         System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n",
                 product.getName(), outOfStockCount);
         return Console.readLine();
     }
 
-    public static String promotionBenefitMessage(Product product) {
+    public static String printPromotionBenefitMessage(Product product) {
+        System.out.println();
         System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", product.getName());
         return Console.readLine();
     }
 
-    public static String membershipMessage() {
+    public static String printMembershipMessage() {
+        System.out.println();
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+    public static String printReorderMessage() {
+        System.out.println();
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
         return Console.readLine();
     }
 }
