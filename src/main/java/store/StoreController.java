237ada0c9f090484339bc720b27621382ec42c0b
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-11 10:41:08 +0900

    refactor: 컨트롤러 수정

--- src/main/java/store/StoreController.java
+++ src/main/java/store/StoreController.java
@@ -1,18 +1,31 @@
 package store;
 
 import java.io.IOException;
+import store.view.InPutView;
+import store.view.OutputView;
 
 public class StoreController {
     public static void run(){
         Inventory inventory = displayInventory();
+        Promotions promotions = displayPromotion();
         OutputView.displayProducts(inventory);
 
         Order order = retryOrder();
+        PromotionStockAlertService psaservice = new PromotionStockAlertService(order,promotions,inventory);
+        OutputView.displayReceipt(psaservice.receipt);
     }
 
     public static Inventory displayInventory() {
         try {
             return new Inventory();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Promotions displayPromotion() {
+        try {
+            return new Promotions();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
