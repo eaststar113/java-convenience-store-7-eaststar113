0600fc7df1a7ebf682882597f7cbef6c0f2489e2
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-11 10:40:40 +0900

    refactor: 영수증 출력과 안내 문구 출력 위한 메서드 생성

--- src/main/java/store/view/InPutView.java
+++ src/main/java/store/view/InPutView.java
@@ -4,11 +4,28 @@ import static store.InputMessage.NEXT_LINE;
 import static store.InputMessage.ORDER_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.Product;
 
 public class InPutView {
 
     public static String getOrder() {
         System.out.println(NEXT_LINE.getMessage()+ORDER_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+
+    public static String promotionStockAlertMessage(Product product, int outOfStockCount) {
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n",
+                product.getName(), outOfStockCount);
+        return Console.readLine();
+    }
+
+    public static String promotionBenefitMessage(Product product) {
+        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", product.getName());
+        return Console.readLine();
+    }
+
+    public static String membershipMessage() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
         return Console.readLine();
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,9 +1,12 @@
 package store.view;
 
+import java.util.Map;
 import store.Inventory;
 import store.Product;
+import store.Receipt;
 
 public class OutputView {
+
     public static void displayProducts(Inventory products) {
         System.out.println("안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.\n");
@@ -11,5 +14,46 @@ public class OutputView {
         for (Product product : Inventory.getProducts()) {
             System.out.println(product);
         }
+    }
+
+    public static void displayReceipt(Receipt receipt) {
+        System.out.println("==============W 편의점================");
+        System.out.println("상품명\t\t수량\t금액");
+
+        for (Map.Entry<Product, Integer> product : receipt.getList().entrySet()) {
+            String name = product.getKey().getName();
+            int price = product.getKey().getPrice();
+            int su = product.getValue();
+            System.out.println(name + "\t\t" + su + "\t" + String.format("%,d", price * su));
+        }
+        System.out.println("=============증\t정===============");
+        for (Map.Entry<Product, Integer> product : receipt.getOnePlusOne().entrySet()) {
+            String name = product.getKey().getName();
+            int su = product.getValue();
+            System.out.println(name+"\t"+"\t"+su+"\t");
+        }
+        System.out.println("====================================");
+        int price = 0;
+        int suSum = 0;
+        for (Map.Entry<Product, Integer> product : receipt.getList().entrySet()) {
+            int perPrice = product.getKey().getPrice();
+            int su = product.getValue();
+            price += perPrice * su;
+            suSum += su;
+        }
+        System.out.println("총구매액\t\t" + suSum + "\t" + String.format("%,d", price));
+        int perPrice = 0;
+        int su = 0;
+        int discountPrice = 0;
+        for (Map.Entry<Product, Integer> product : receipt.getOnePlusOne().entrySet()) {
+            perPrice = product.getKey().getPrice();
+            su = product.getValue();
+            discountPrice += perPrice*su;
+        }
+        System.out.println("행사할인\t\t\t\t" + "-" + String.format("%,d", discountPrice));
+        System.out.println("멤버십 할인\t\t\t\t" + "-" + String.format("%,d", receipt.getMembershipDiscount()));
+
+        int finalPrice = price - discountPrice - receipt.getMembershipDiscount();
+        System.out.println("내실돈\t\t\t\t" + String.format("%,d", finalPrice));
     }
 }
