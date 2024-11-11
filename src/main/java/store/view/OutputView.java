1a0cabe573f30132ffac5692692514e1e684c19b
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-11 18:19:17 +0900

    refactor: 영수증 출력 형식 메서드 분리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -20,40 +20,50 @@ public class OutputView {
         System.out.println("==============W 편의점================");
         System.out.println("상품명\t\t수량\t금액");
 
+        printProductItems(receipt);
+        System.out.println("=============증\t정===============");
+        printBenefitItems(receipt);
+        System.out.println("====================================");
+        printPriceInfo(receipt);
+    }
+
+    private static void printPriceInfo(Receipt receipt) {
+        printTotalPrice(receipt);
+        printDiscountPrice(receipt);
+        printFinalPrice(receipt);
+    }
+
+    private static void printProductItems(Receipt receipt) {
         for (Map.Entry<Product, Integer> product : receipt.getList().entrySet()) {
             String name = product.getKey().getName();
             int price = product.getKey().getPrice();
             int su = product.getValue();
             System.out.println(name + "\t\t" + su + "\t" + String.format("%,d", price * su));
         }
-        System.out.println("=============증\t정===============");
+    }
+
+    private static void printBenefitItems(Receipt receipt) {
         for (Map.Entry<Product, Integer> product : receipt.getOnePlusOne().entrySet()) {
             String name = product.getKey().getName();
             int su = product.getValue();
             System.out.println(name+"\t"+"\t"+su+"\t");
         }
-        System.out.println("====================================");
-        int price = 0;
-        int suSum = 0;
-        for (Map.Entry<Product, Integer> product : receipt.getList().entrySet()) {
-            int perPrice = product.getKey().getPrice();
-            int su = product.getValue();
-            price += perPrice * su;
-            suSum += su;
-        }
+    }
+
+    private static void printTotalPrice(Receipt receipt) {
+        int suSum = receipt.calculateTotalQuantity();
+        int price = receipt.calculateTotalPrice();
         System.out.println("총구매액\t\t" + suSum + "\t" + String.format("%,d", price));
-        int perPrice = 0;
-        int su = 0;
-        int discountPrice = 0;
-        for (Map.Entry<Product, Integer> product : receipt.getOnePlusOne().entrySet()) {
-            perPrice = product.getKey().getPrice();
-            su = product.getValue();
-            discountPrice += perPrice*su;
-        }
+    }
+
+    private static void printDiscountPrice(Receipt receipt) {
+        int discountPrice = receipt.calculateDiscountPrice();
         System.out.println("행사할인\t\t\t\t" + "-" + String.format("%,d", discountPrice));
         System.out.println("멤버십 할인\t\t\t\t" + "-" + String.format("%,d", receipt.getMembershipDiscount()));
+    }
 
-        int finalPrice = price - discountPrice - receipt.getMembershipDiscount();
+    private static void printFinalPrice(Receipt receipt) {
+        int finalPrice = receipt.calculateFinalPrice();
         System.out.println("내실돈\t\t\t\t" + String.format("%,d", finalPrice));
     }
 }
